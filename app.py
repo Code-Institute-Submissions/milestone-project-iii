e4f70e4e6ffab205d9d97480812a04dafe6262c9
Aman Gurung <creates.creation@gmail.com>
2020-09-28 16:53:36 +0000

    Added edit_thought.html,created route to edit_thought.html, created update or editing route to edit button and links to buttons and links

--- app.py
+++ app.py
@@ -15,16 +15,36 @@ mongo = PyMongo(app)
 @app.route('/')
 @app.route('/get_thoughts')
 def get_thoughts():
-    return render_template("get_thoughts.html", thoughts = mongo.db.thoughts.find())
+    return render_template("get_thoughts.html", thoughts=mongo.db.thoughts.find())
+
 
 @app.route('/add_thought')
 def add_thought():
     return render_template("add_thought.html", thoughts=mongo.db.thoughts.find())
 
+
 @app.route('/insert_thoughts', methods=['POST'])
 def insert_thoughts():
-    thoughts =  mongo.db.thoughts
+    thoughts = mongo.db.thoughts
     thoughts.insert_one(request.form.to_dict())
+    return redirect(url_for('get_thoughts'))
+
+
+@app.route('/edit_thought/<thought_id>')
+def edit_thought(thought_id):
+    the_thought = mongo.db.thoughts.find_one({"_id": ObjectId(thought_id)})
+    return render_template('edit_thought.html', thought=the_thought)
+
+
+@app.route('/update_thought/<thought_id>', methods=["POST"])
+def update_thought(thought_id):
+    thoughts = mongo.db.thoughts
+    thoughts.update({'_id': ObjectId(thought_id)},
+                    {
+        'title': request.form.get('title'),
+        'content': request.form.get('content'),
+        'name': request.form.get('name'),
+    })
     return redirect(url_for('get_thoughts'))
 
 
